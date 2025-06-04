34c7c4b9491565e28ca53a555e6464310c90be1d
Jai Bhullar <81278507+JaiBh@users.noreply.github.com>
2025-06-04 03:06:56 +0100

    fix webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -61,7 +61,6 @@ export async function POST(req: Request) {
         isArchived: true,
       },
     });
-
-    return new NextResponse(null, { status: 200 });
   }
+  return new NextResponse(null, { status: 200 });
 }
