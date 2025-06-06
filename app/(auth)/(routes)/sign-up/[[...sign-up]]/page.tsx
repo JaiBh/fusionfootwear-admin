501ca3d8366a2198c11d22925755364d5cfc8157
Jai Bhullar <81278507+JaiBh@users.noreply.github.com>
2025-06-06 03:31:01 +0100

    fix sign up redirect bug

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -4,6 +4,6 @@ import { useSearchParams } from "next/navigation";
 function SignUpPage() {
   const searchParams = useSearchParams();
   const redirectUrl = searchParams.get("redirect") || "/";
-  return <SignUp></SignUp>;
+  return <SignUp forceRedirectUrl={redirectUrl}></SignUp>;
 }
 export default SignUpPage;
